6da7818ed073db42348cb14cb9af6e777307caa9
shoumik <yeahea.hoque@gmail.com>
2023-10-06 21:11:48 +0600

    previously solved problems

---
+++ DP/152_maximum_product_subarray.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def maxProduct(nums: List[int]) -> int:
+        return 0
+
+
+if __name__ == '__main__':
+    numbers = [2, 3, -2, 4]
+    print(Solution().maxProduct(numbers))
