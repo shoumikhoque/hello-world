eddec16e5b31669977e0c37bcdc8e7e57b695cfa
shoumik <yeahea.hoque@tallykhata.com>
2024-05-12 17:09:21 +0600

    Trie-> get sorted word list

--- concept/Trie/Trie.py
+++ concept/Trie/Trie.py
@@ -137,6 +137,28 @@ class Trie:
                 result+=self.total_Number_of_words(letter)
         return result
 
+    def get_words(root, result, level, word):
+
+        # Leaf denotes end of a word
+        if root.is_end_word:
+            # current word is stored till the 'level' in the character array
+            temp = ""
+            for x in range(level):
+                temp += word[x]
+            result.append(str(temp))
+
+        for i in range(26):
+            if root.children[i]:
+                # Non-None child, so add that index to the character array
+                word[level] = chr(i + ord('a'))  # Add character for the level
+                self.get_words(root.children[i], result, level + 1, word)
+
+    def find_words(root):
+        result = []
+        word = [None] * 20  # assuming max level is 20
+        get_words(root, result, 0, word)
+        return result
+
 
 if __name__ == '__main__':
     # Input keys (use only 'a' through 'z')
@@ -161,3 +183,4 @@ if __name__ == '__main__':
     print("Deleted key \"abc\" \n")
     print(t.total_Number_of_words(t.root))
     print("abc --- " + res[1] if t.search("abc") else "abc --- " + res[0])
+    t.find_words()
