d58fea7b30a1b702a3e9f4b4075011bb84c6fb00
shoumik <yeahea.hoque@tallykhata.com>
2024-06-09 15:44:53 +0600

    detect cycle

--- concept/Graph/detect_cycle.py
+++ concept/Graph/detect_cycle.py
@@ -1,32 +1,47 @@
 from concept.Graph.Graph import Graph
-def detect_cycle(g, param):
-    # traverse using DFS
-    # if any node is found which already has visited[val]=True
-    # this node is one of the answers which has created the cycle
-    pass
+
+
+def detect_cycle_rec(g, node, visited, rec_node_stack):
+    # Node was already in the recursion stack. Cycle found.
+    if rec_node_stack[node]:
+        return True
+    if visited[node]:
+        return False
+    # Mark current node as visited and add to recursion stack
+    visited[node] = True
+    rec_node_stack[node] = True
+    adj_list=g.adj_list[node]
+    if adj_list is not None:
+        current = adj_list.head
+        while current is not None:
+            # Pick adjacent node and call it recursively
+            adj_node_val = current.val
+            # If the node is visited again in the same recursion => Cycle found
+            if detect_cycle_rec(g, adj_node_val, visited, rec_node_stack):
+                return True
+            current = current.next
+    rec_node_stack[node] = False
+    return False
+
+
+def detect_cycle(g):
+    visited=[False]* g.nodes
+    rec_node_stack=[False]* g.nodes
+    for node in range (g.nodes):
+        if detect_cycle_rec(g,node,visited,rec_node_stack):
+            return True
+    return False
 
 
 if __name__ == '__main__':
-    g=Graph(10)
-    g.add_edge(0,1)
-    g.add_edge(0,2)
-    g.add_edge(1,3)
-    g.add_edge(1, 5)
-    g.add_edge(2, 4)
-    g.add_edge(2, 6)
-    g.add_edge(3, 7)
-    g.add_edge(3, 8)
-    g.add_edge(4, 9)
-    g.add_edge(4, 5)
-    g.add_edge(5, 7)
-    g.add_edge(5, 6)
-    g.add_edge(6, 8)
-    g.add_edge(6, 9)
-    g.add_edge(7,0)
-    g.add_edge(8, 5)
-    g.add_edge(7, 9)
-    g.add_edge(9, 3)
-    g.add_edge(9, 5)
-
-    # print(str(g))
-    print(detect_cycle(g,0))
+    edges_list = [[[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6],[6, 2] ]]
+    i = 0
+    for i in range(len(edges_list)):
+        num_vertices = 7
+        g = Graph(num_vertices)
+        for edge in edges_list[i]:
+            g.add_edge(edge[0],edge[1])
+        print(str(g))
+        print("\n\t Output:", detect_cycle(g))
+        print("-" * 100)
+        i += 1
