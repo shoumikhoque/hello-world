22f669d5fbbe7c4c63238600d299a8c2a451588e
shoumik <yeahea.hoque@tallykhata.com>
2025-02-06 17:03:50 +0600

    Pattern DP: coin change memoization

--- coding-interview-patterns/13-DP/coin_change.py
+++ coding-interview-patterns/13-DP/coin_change.py
@@ -1,10 +1,13 @@
+from decimal import Decimal
+
+
 def coin_change(coins, total,dp):
     if total<=0:
         return 0
-    if dp[total]!= float('inf')
+    if dp[total]== Decimal('Infinity'):
         for i in coins:
             if i<=total:
-                dp[i]=min(dp[i],1+coin_change(coins,total-i,dp[i]))
+                dp[total]=min(dp[total],1+coin_change(coins,total-i,dp))
     return dp[total]
 
 
@@ -12,5 +15,5 @@ if __name__ == '__main__':
     coins = [1, 2, 4, 5]
     coins.reverse()
     total = 11
-    dp=[float('inf') for x in range(total+1)]
+    dp=[Decimal('Infinity') for x in range(total+1)]
     print(coin_change(coins,total,dp))
