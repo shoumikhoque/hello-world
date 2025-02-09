72e827fa891c199c27887b18803466a25f48c6e2
shoumik <yeahea.hoque@tallykhata.com>
2025-02-09 17:04:24 +0600

    Pattern DP: coin change 2 brute force

--- coding-interview-patterns/13-DP/coin_change_2.py
+++ coding-interview-patterns/13-DP/coin_change_2.py
@@ -1,17 +1,19 @@
-def calc(amount,coins,pos,ans):
-    if amount == 0:
-        return ans + 1
-    if amount < 0 or pos >= len(coins):
-        return 0
-    for i in range(pos, len(coins)):
-        if coins[i] <= amount:
-            ans = calc(amount - coins[i], coins, i, ans)
-    return ans
+def change(amount, coins) :
+    def calc(amount, pos, ans):
+        if amount == 0:
+            return ans + 1
+        if amount < 0 or pos >= len(coins):
+            return 0
+        for i in range(pos, len(coins)):
+            if coins[i] <= amount:
+                ans = calc(amount - coins[i], i, ans)
+        return ans
+    return calc(amount, 0, 0)
 
 
 
 if __name__ == '__main__':
-    amount = 10
-    coins = [10]
+    amount = 5
+    coins = [1,2,5]
     coins.reverse()
-    print(calc(amount,coins,0,0))
+    print(change(amount,coins,))
