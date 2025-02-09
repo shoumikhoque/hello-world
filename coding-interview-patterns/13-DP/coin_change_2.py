b1651faa1a5f6ce6ebb61328c70d85e424016bd5
shoumik <yeahea.hoque@tallykhata.com>
2025-02-09 17:11:44 +0600

    Pattern DP: coin change 2 memoization

--- coding-interview-patterns/13-DP/coin_change_2.py
+++ coding-interview-patterns/13-DP/coin_change_2.py
@@ -1,14 +1,17 @@
 def change(amount, coins) :
-    def calc(amount, pos, ans):
+    dp = [[0] * len(coins) for x in range(amount + 1)]
+    def calc(amount, pos):
         if amount == 0:
-            return ans + 1
+            return dp[amount][pos] + 1
         if amount < 0 or pos >= len(coins):
             return 0
-        for i in range(pos, len(coins)):
-            if coins[i] <= amount:
-                ans = calc(amount - coins[i], i, ans)
-        return ans
-    return calc(amount, 0, 0)
+        if dp[amount][pos]==0:
+            for i in range(pos, len(coins)):
+                if coins[i] <= amount:
+                    dp[amount][pos] = calc(amount - coins[i], i)
+        return dp[amount][pos]
+    calc(amount, 0)
+    return sum(dp[amount])
 
 
 
@@ -16,4 +19,4 @@ if __name__ == '__main__':
     amount = 5
     coins = [1,2,5]
     coins.reverse()
-    print(change(amount,coins,))
+    print(change(amount,coins))
