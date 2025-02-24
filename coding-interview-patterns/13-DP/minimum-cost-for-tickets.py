6689af782b909252b1312a3b4c226638443cd397
shoumik <yeahea.hoque@tallykhata.com>
2025-02-24 16:18:09 +0600

    Pattern DP:

--- coding-interview-patterns/13-DP/minimum-cost-for-tickets.py
+++ coding-interview-patterns/13-DP/minimum-cost-for-tickets.py
@@ -1,3 +1,6 @@
+
+
+
 def solve(days, costs):
     costs={
         1:costs[0],
@@ -5,11 +8,29 @@ def solve(days, costs):
         30:costs[2]
     }
     dp={}
+
+    def next_index_for_30_days(index):
+        ans=days[index]
+        for i in range(index+1,len(days)):
+            if ans>days[index]+29:
+                break
+            ans=days[i]
+        return ans
+
+    def next_index_for_7_days(index):
+        ans = days[index]
+        for i in range(index+1, len(days)):
+            if ans > days[index] + 6:
+                break
+            ans = days[i]
+        return ans
     def calc(index):
         if index>=len(days):
             return 0
-        if days[index]
-    calc(0)
+        result=costs[0]+calc(index+1)
+        result=min( result,costs[1]+calc(next_index_for_7_days(index)),30+ costs[2]+calc(next_index_for_30_days(index)))
+        return result
+    return calc(0)
 
 
 
