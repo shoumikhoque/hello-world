376bbc85f16f5cf827ade299445d12378ca9357a
shoumik <yeahea.hoque@tallykhata.com>
2025-04-10 15:27:42 +0600

    Pattern DP: Longest common subsequence

--- leetcode/DP/longest_common_subsequence.py
+++ leetcode/DP/longest_common_subsequence.py
@@ -19,6 +19,8 @@ def solve_table(s1,s2):
     n1=len(s1)
     n2=len(s2)
     dp=[[0]*(n2+1) for _ in range(n1+1)]
+    dp1=[0]*(n1+1)
+    dp2= [0] * (n2 + 1)
     for i in range(n1-1,-1,-1):
         for j in range(n2-1,-1,-1):
             if s1[i]==s2[j]:
