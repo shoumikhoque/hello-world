be300e366f8d933761840512553baa6de734157f
shoumik <yeahea.hoque@tallykhata.com>
2025-06-02 13:14:32 +0600

    Pattern Greedy: longest-arithmetic-subsequence

--- coding-interview-patterns/13-DP/partition_equal_subset_sum.py
+++ coding-interview-patterns/13-DP/partition_equal_subset_sum.py
@@ -1,11 +1,18 @@
-def solve(nums):
-
-    def calc(pos):
-        if pos==len(nums):
-            re
-
-
-
+def solve_bottom_up(nums):
+    total=sum(nums)
+    if total %2==1:
+        return False
+    total=total//2
+    nums.sort()
+    dp= [[True]* len(nums)] + [[False]*len(nums) for i in range(total)]
+    for target in range(1,total+1):
+        for i in range(len(nums)):
+            if nums[i]==target:
+                dp[target][i]=True
+                continue
+            if i>0:
+                dp[target][i]= dp[target][i-1] or (target-nums[i]>=0 and dp[target-nums[i]][i-1])
+    return dp[total][len(nums)-1]
 if __name__ == '__main__':
-    nums=[1,5,11,5]
-    print(solve(nums))
+    nums=[100,4,6]
+    print(solve_bottom_up(nums))
