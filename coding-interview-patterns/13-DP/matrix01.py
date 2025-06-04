77e1ec4e6216fd204fbbdf241acf0ca3e00967ff
shoumik <yeahea.hoque@tallykhata.com>
2025-06-04 09:33:14 +0600

    matrix 01

--- coding-interview-patterns/13-DP/matrix01.py
+++ coding-interview-patterns/13-DP/matrix01.py
@@ -1,33 +1,45 @@
+from collections import deque
+
+
 def solve(mat):
-    m = len(mat)
-    n = len(mat[0])
-    if m == 1 and n == 1:
-        return mat
+    if not mat or not mat[0]:
+        return []
+
+    m, n = len(mat), len(mat[0])
+    queue = deque()
+    MAX_VALUE = m * n
+
+    # Initialize the queue with all 0s and set cells with 1s to MAX_VALUE.
     for i in range(m):
         for j in range(n):
-            if mat[i][j] == 1:
-                temp = 100000
-                if i - 1 >= 0:
-                    temp = min(temp, mat[i - 1][j])
-                if j - 1 >= 0:
-                    temp = min(temp, mat[i][j - 1])
-                if i + 1 < m:
-                    temp = min(temp, mat[i + 1][j])
-                if j + 1 < n:
-                    temp = min(temp, mat[i][j + 1])
-                mat[i][j] =1+ temp
+            if mat[i][j] == 0:
+                queue.append((i, j))
+            else:
+                mat[i][j] = MAX_VALUE
+
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+    while queue:
+        row, col = queue.popleft()
+        for dr, dc in directions:
+            r, c = row + dr, col + dc
+            if 0 <= r < m and 0 <= c < n and mat[r][c] > mat[row][col] + 1:
+                queue.append((r, c))
+                mat[r][c] = mat[row][col] + 1
+
     return mat
 
 
 if __name__ == '__main__':
-    mat = [[1, 0, 1, 1, 0, 0, 1, 0, 0, 1],
-           [0, 1, 1, 0, 1, 0, 1, 0, 1, 1],
-           [0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
-           [1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
-           [0, 1, 0, 1, 1, 0, 0, 0, 0, 1],
-           [0, 0, 1, 0, 1, 1, 1, 0, 1, 0],
-           [0, 1, 0, 1, 0, 1, 0, 0, 1, 1],
-           [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
-           [1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
-           [1, 1, 1, 1, 0, 1, 0, 0, 1, 1]]
+    # mat = [[1, 0, 1, 1, 0, 0, 1, 0, 0, 1],
+    #        [0, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+    #        [0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+    #        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+    #        [0, 1, 0, 1, 1, 0, 0, 0, 0, 1],
+    #        [0, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+    #        [0, 1, 0, 1, 0, 1, 0, 0, 1, 1],
+    #        [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    #        [1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+    #        [1, 1, 1, 1, 0, 1, 0, 0, 1, 1]]
+    mat=[[0,0,0],[0,1,0],[1,1,1]]
     print(solve(mat))
